a5eba98fcc9125f833d554775b989e46e11b8e08
Justin <justinhammond859@yahoo.com>
2015-06-22 17:58:15 +0200

    added a few gems

--- blogger/app/helpers/articles_helper.rb
+++ blogger/app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   def article_params
-    params.require(:article).permit(:title, :body, :tags_list)
+    params.require(:article).permit(:title, :body, :tags_list, :image)
   end
 
 end
